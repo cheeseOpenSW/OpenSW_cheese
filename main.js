5db56130e893e6aadcd9266e7e083b5f71768456
handagyeong <80438961+handagyeong@users.noreply.github.com>
2023-11-28 00:53:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const pool = mysql.createPool({
 });
 
 module.exports = pool;  //모듈로 내보내기
-
+//test
 // 스케줄링을 위한 패키지 추가
 const schedule = require('node-schedule');
 require('dotenv').config({path: "./config/sens.env"}); // sens.env 불러오기
